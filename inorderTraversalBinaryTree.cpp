5a1ceeca2e92600be433c812465805b43e0d6bf9
Deepanshi Sharma <120949758+Deepanshi-17@users.noreply.github.com>
2024-08-27 17:11:13 +0530

    Create inorderTraversalBinaryTree.cpp

---
+++ inorderTraversalBinaryTree.cpp
@@ -0,0 +1,18 @@
+/*TC -> O(N)
+  SC -> O(N)
+  */
+class Solution {
+public:
+    void fun(TreeNode* root, vector<int>& ans)
+    {
+        if(root == NULL) return;
+        fun(root->left, ans);
+        ans.push_back(root->val);
+        fun(root->right, ans);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        fun(root, ans);
+        return ans;
+    }
+};
