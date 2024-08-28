655dcdcba8def55c02cd4babbb64732ff97482e3
Deepanshi Sharma <120949758+Deepanshi-17@users.noreply.github.com>
2024-08-28 14:59:09 +0530

    Create kthSmallestElement.cpp

---
+++ kthSmallestElement.cpp
@@ -0,0 +1,19 @@
+/*TC -> O(nlogk)
+  SC -> O(k)
+  */
+
+class Solution {
+  public:
+    // arr : given array
+    // k : find kth smallest element and return using this function
+    int kthSmallest(vector<int> &arr, int k) {
+        priority_queue<int>q;
+        for(int i = 0; i < arr.size(); i++)
+        {
+            q.push(arr[i]);
+            if(q.size() > k)
+            q.pop();
+        }
+        return q.top();
+    }
+};
