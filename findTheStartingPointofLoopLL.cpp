129bf329f7f46b02deb6059192c054371cf21cfb
Deepanshi Sharma <120949758+Deepanshi-17@users.noreply.github.com>
2024-08-29 00:34:18 +0530

    Create findTheStartingPointofLoopLL.cpp

---
+++ findTheStartingPointofLoopLL.cpp
@@ -0,0 +1,27 @@
+/*TC -> O(N)
+  SC -> O(1)
+  */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head == NULL || head->next == NULL) return NULL;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast->next && fast->next->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast)
+            {
+                slow = head;
+                while(slow != fast)
+                {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
+        }
+        return NULL;
+    }
+};
