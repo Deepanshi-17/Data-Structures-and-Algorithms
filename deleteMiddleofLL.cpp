914eb94c1f89a384e7315faeac9c065a0ea54281
Deepanshi Sharma <120949758+Deepanshi-17@users.noreply.github.com>
2024-08-29 00:58:06 +0530

    Create deleteMiddleofLL.cpp

---
+++ deleteMiddleofLL.cpp
@@ -0,0 +1,24 @@
+/* TC -> O(N)
+ SC -> O(1)
+*/
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if(head == NULL || head->next == NULL) return NULL;
+        if(head->next->next == NULL)
+        {
+            head->next = NULL;
+            return head;
+        };
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        slow->val = slow->next->val;
+        slow->next = slow->next->next;
+        return head;
+    }
+};
